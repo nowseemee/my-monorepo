c255b054ba75540106b879e6c5b2b11b1ba0ce5e
Daniel Beck <daniel.beck@hubrick.com>
2018-05-14 01:30:27 +0200

    have a server flag

--- packages/webapp/server/index.js
+++ packages/webapp/server/index.js
@@ -3,6 +3,7 @@ const yt = require('./yt');
 const app = require('./app');
 
 global.XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+global.isServer = true;
 
 exports.app = functions.https.onRequest(app);
 exports.yt = functions.https.onRequest(yt);
