35efd4396c431cddb16bc139620fb033db838a7b
Daniel Beck <daniel.beck@hubrick.com>
2018-05-13 21:54:01 +0200

    remove googleapis caching

--- packages/webapp/public/sw.js
+++ packages/webapp/public/sw.js
@@ -1,16 +1,3 @@
 self.importScripts('sw-toolbox.js');
 
-toolbox.router.get('/(.*)', toolbox.networkFirst, {
-    // Use a dedicated cache for the responses, separate from the default cache.
-    cache: {
-        name: 'googleApis',
-        // Store up to 10 entries in that cache.
-        maxEntries: 10,
-        // Expire any entries that are older than 30 seconds.
-        maxAgeSeconds: 30000,
-    },
-
-    origin: /\.googleapis\.com$/,
-});
-
 self.importScripts('service-worker.js');
