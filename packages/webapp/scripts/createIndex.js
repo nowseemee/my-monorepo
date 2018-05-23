4912df013bc98cb67f5bca057ebcb39d4f7393fe
Daniel Beck <daniel.beck@hubrick.com>
2018-05-23 12:50:26 +0200

    move to scripts/

--- packages/webapp/scripts/createIndex.js
+++ packages/webapp/scripts/createIndex.js
@@ -1,4 +1,4 @@
-const renderToHtml = require('./server/renderToHtml');
+const renderToHtml = require('../server/renderToHtml');
 const fs = require('fs');
 
 renderToHtml('/').then((html) => fs.writeFile('./build/index.html', html));
